03b90faf4366c7c98cd9d22f0f66ddb5550b887c
notGabo <ga.soto@duocuc.cl>
2024-09-16 20:39:42 -0300

    UPD: Remover experimentaciones con api

--- frontend/src/app/api/test/[test]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET(request: Request, { params }: { params: { test: string } }) {
-  return NextResponse.json({ message: `Hello ${params.test}` });
-}
-
-export function POST(request: Request, { params }: { params: { test: string } }) {
-  return NextResponse.json({ message: `Hello ${params.test}` });
-}
