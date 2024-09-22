fbd689a5f9b929bb9a7c54bb251e6d0ce34efe66
notGabo <ga.soto@duocuc.cl>
2024-09-22 02:44:39 -0300

    UPD: Crear pagina dispositivos

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -67,7 +67,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center flex-col space-y-4 p-6 pt-0", className)}
+    className={cn("flex items-center p-6 pt-0", className)}
     {...props}
   />
 ))
