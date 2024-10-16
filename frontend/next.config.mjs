d4893b7924a8e5192eefa125ecf01deef5c2b200
notGabo <ga.soto@duocuc.cl>
2024-10-15 21:10:07 -0300

    UPD: Borrar carpeta frontend

--- frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-});
-
-export default withPWA({
-  // Your Next.js config
-});
--- frontend/src/app/(home)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
-
-export default function DemoLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
-  return <AdminPanelLayout>{children}</AdminPanelLayout>;
-}
