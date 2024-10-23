a088983cd37876bb3047cd51a2726457804d92ed
notGabo <ga.soto@duocuc.cl>
2024-10-22 21:20:36 -0300

    UPD: Preparar ambientes

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Card,
   CardContent,
@@ -7,12 +8,42 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+
 import Image from "next/image";
 import FooterButtons from "@/components/auth/FooterButtons";
 
+import {FormEvent} from 'react';
+//import {useRouter} from 'next/compat/router';
+// import {useRouter} from 'next/router';
+import {useRouter} from 'next/navigation';
+
+import React from "react";
+
+
+
 export function LoginForm() {
+
+  const router = useRouter();
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+    {response.ok ? router.push('/dashboard') : alert('Error al iniciar sesión')}
+    }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
       <Card className="w-full max-w-sm">
         <Image
           src="/icon-128x128.png"
@@ -33,6 +64,8 @@ export function LoginForm() {
             <Input
               id="email"
               type="email"
+              name="email"
+              // onChange={handleChange}
               placeholder="correo@ejemplo.com"
               required
             />
@@ -42,6 +75,8 @@ export function LoginForm() {
             <Input
               id="password"
               type="password"
+              name="password"
+              // onChange={handleChange}
               placeholder="********"
               required
             />
@@ -49,6 +84,7 @@ export function LoginForm() {
         </CardContent>
         <FooterButtons textoBotonPrincipal="Iniciar Sesión" />
       </Card>
+      </form>
     </>
-  );
-}
+    );
+  }
